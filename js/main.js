b591811910d85ccc9e4608c8b917b96b8fdf17b3
MuhammadjonIsmoiljonov <muhammadjonismoiljonov0@gmail.com>
2021-11-08 10:11:30 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,60 @@
+        //  1-usul
+// var sayohat = prompt("sayohatga sarflaydigan pulingizni kiriting");
+
+// if (sayohat>=500* 9433.34+250* 9433.34 ){
+// console.log("sayohatga borishingiz mumkin");
+// alert("sayohatga borishingiz mumkin");
+// }else{
+//   alert("sayohatga borish ucun pulingiz yetmaydi")
+//   console.log("sayohatga borish ucun pulingiz yetmaydi");
+// }
+
+
+
+         // 2-usul;
+// var sayohat = prompt("sayohatga sarflaydigan pulingizni kiriting");
+// var FLIGHTCOST=500*9433.34;
+// var HOTELCOST=250*9433.34;
+// var MUSEUMANDOTHERS=500*10354.03;
+
+// if(sayohat>=FLIGHTCOST+HOTELCOST+MUSEUMANDOTHERS){
+//   alert("sayohatga borishing mumkin Alisher")
+// console.log("sayohatga borishing mumkin Alisher");
+// }else{
+//   alert("sayohatga borishing ucun puling yetmaydi")
+//   console.log("sayohatga borishing ucun puling yetmaydi");
+// };
+
+        //  3-usul
+
+var sayohat = prompt("sayohatga sarflaydigan pulingizni kiriting");
+var FLIGHTCOST=500*9433.34;
+var HOTELCOST=250*9433.34;
+var MUSEUMANDOTHERS=500*10354.03;
+var umumiy=FLIGHTCOST+HOTELCOST+MUSEUMANDOTHERS
+if(sayohat>= umumiy){
+  alert("sayohatga borishing mumkin Alisher")
+console.log("sayohatga borishing mumkin Alisher");
+}else{
+  alert("sayohatga borishing ucun puling yetmaydi")
+  console.log("sayohatga borishing ucun pulingiz yetmaydi");
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
